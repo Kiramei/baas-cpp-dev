166072e66cb7d070e895e8ae9b9ecde7cf71ad8a
pur1fy <2274916027@qq.com>
2026-06-08 21:42:44 +0800

    conan: add spdlog private recipe

---
+++ deploy/conan/recipes/baas-spdlog/conanfile.py
@@ -0,0 +1,39 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import collect_libs, get
+
+
+class BAASSpdlogConan(ConanFile):
+    name = "baas-spdlog"
+    version = "1.15.3"
+    license = "MIT"
+    package_type = "shared-library"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def source(self):
+        source = self.conan_data["sources"][str(self.version)]
+        get(self, url=source["url"], sha256=source["sha256"], strip_root=True, destination=self.source_folder)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        for key, value in self.conan_data["build"]["cmake_options"].items():
+            tc.variables[key] = str(value)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "baas-spdlog")
+        self.cpp_info.set_property("cmake_target_name", "BAAS::spdlog")
+        self.cpp_info.libs = collect_libs(self)
+        self.cpp_info.bindirs = ["bin"]
